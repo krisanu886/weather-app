0ec053c1db46fc4e52f160077b4a77fda44ec0d2
Krisanu <kisanudey886@gmail.com>
2022-03-15 20:21:20 +0530

    creating....

--- src/components/WeatherApp.js
+++ src/components/WeatherApp.js
@@ -1,22 +1,48 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../App.css";
 
 const WeatherApp = () => {
-    const [city, setCity] = useState(null);
-    const [search, setSearch] =useState('Kolkata');
+  const [city, setCity] = useState(null);
+  const [search, setSearch] = useState("Kolkata");
+
+  useEffect(() => {
+    const fetchApi = async () => {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=1e1840fd674677602856deed77597fd0`;
+      const response = await fetch(url);
+
+      const resJson = await response.json();
+      // console.log(resJson);
+      setCity(resJson.main);
+    };
+
+    fetchApi();
+  }, [search]);
   return (
     <>
       <div className="box">
         <div className="inputData">
           <h1> Weather App </h1>
-          <input type="search" className="inputFields" onChange={() => {}} />
+          <input
+            type="search"
+            className="inputFields"
+            placeholder="Kolkata"
+            onChange={(event) => {
+              setSearch(event.target.value);
+            }}
+          />
         </div>
 
-        <div className="info">
-          <h2 className="location">{city}</h2>
-          <h1 className="temp">5.23 cel</h1>
-          <h3 className="temp-min">Min:5.23 cel | max: 5.25 cel</h3>
-        </div>
+        {!city ? (
+          <p>Type your correct Location</p>
+        ) : (
+          <div>
+            <div className="info">
+              <h2 className="location">{search}</h2>
+              <h1 className="temp">{city.temp} °Cel</h1>
+              <h3 className="temp-min">Min: {city.temp_min} °Cel | Max: {city.temp_max} °Cel</h3>
+            </div>
+          </div>
+        )}
       </div>
     </>
   );
